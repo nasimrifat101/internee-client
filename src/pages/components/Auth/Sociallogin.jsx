2a0c3ee90c5ec6fe262a7698cdd5032f3d421323
nasimrifat101 <nasimrifat101@gmail.com>
2023-12-18 01:10:57 +0600

    login page and social login done

--- src/pages/components/Auth/Sociallogin.jsx
+++ src/pages/components/Auth/Sociallogin.jsx
@@ -1,8 +1,26 @@
+import useAuth from "../../../hooks/useAuth";
+import { FaGoogle } from "react-icons/fa";
 
 const Sociallogin = () => {
+    const { signInWithGoogle } = useAuth()
+
+    const handleGoogle = () => {
+        signInWithGoogle()
+            .then(res => {
+                console.log(res.user)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
     return (
         <div>
-            
+            <button
+                onClick={handleGoogle}
+                className="btn w-full">
+                <FaGoogle />
+                SignUp with Google
+            </button>
         </div>
     );
 };
